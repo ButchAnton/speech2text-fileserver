097418eac34e323455294795e31104fca3566129
Butch Anton <butch@butch.net>
2013-01-11 13:18:32 -0800

    Still trying to find ffmpeg.

--- app/controllers/sounds_controller.rb
+++ app/controllers/sounds_controller.rb
@@ -105,6 +105,8 @@ class SoundsController < ApplicationController
     ls_results = system("ls -l #{sound_file_path}")
     puts "sound_file_path = #{sound_file_path}: #{ls_results}"
     puts "which ffmpeg: #{system("which ffmpeg")}"
+    puts "ls -l ffmpeg: #{system("ls -l vendor/ffmpeg-heroku/bin")}"
+    puts "echo $PATH: #{system("echo $PATH")}"
     # puts "ls -R: #{system("ls -R")}"
     audio = Speech::AudioToText.new(sound_file_path, :verbose => true)
     if audio
